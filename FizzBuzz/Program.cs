0689f269b8cefadbbaa78a4c43019a14170cfa98
Lorraine Phillips <lorraine.phillips@softwire.com>
2018-09-05 15:26:28 +0100

    FizzBuzz Part1

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,6 +10,26 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
